5774f476b54a0aba323cac6254c00ac34502ffc3
jzf21 <jozefneelamkavil2003@gmail.com>
2023-08-08 20:01:36 +0530

    ui updates

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -13,27 +13,37 @@ type Props = {
   imageUrl: string,
 };
 
+
 const ProductCard = (props: Props) => {
   const supabaseurl=process.env.NEXT_PUBLIC_SUPABASE_URL
   return (
-    <div className="flex flex-col">
-      
-      <div className="">
     
-        <img src={props.imageUrl} alt="" className=" h-[300px] object-contain" />
-       <div className="flex flex-col">
-          <h1 className="text-lg text-gray-500 ">{props.title}</h1>
-        <p className="text-xl  text-gray-700">${props.price}</p>
-        </div>
-        
-        
-        {/* <p className="text-sm text-gray-500">{props.size}</p>
-        <p className="text-sm text-gray-500">{props.quantity}</p>
-        <p className="text-sm text-gray-500">{props.rating}</p>
-        <p className="text-sm text-gray-500">{props.price}</p> */}
-        
-      </div>
+      <div  className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+  <a className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+    <img className="object-cover" src={props.imageUrl} alt="product image" />
+    <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
+  </a>
+  <div className="mt-4 px-5 pb-5">
+    <a href="#">
+      <h5 className="text-xl tracking-tight text-slate-900">{props.title}</h5>
+    </a>
+    <div className="mt-2 mb-5 flex items-center justify-between">
+      <p>
+        <span className="text-3xl font-bold text-slate-900">₹{props.price}</span>
+        <span className="text-sm text-slate-900 line-through">{props.price}</span>
+      </p>
+     
     </div>
+    <a href="#" className="flex items-center justify-center rounded-md bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+      <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+      </svg>
+      Delete
+    </a>
+  </div>
+</div>
+
+   
 
   );
 };
--- components/ProductCard/ProductCardBuyer.tsx
+++ components/ProductCard/ProductCardBuyer.tsx
@@ -16,26 +16,31 @@ type Props = {
 const ProductCardBuyer = (props: Props) => {
   const supabaseurl=process.env.NEXT_PUBLIC_SUPABASE_URL
   return (
-    <div className="bg-white shadow-md rounded-md p-4">
-      
-      <div className="">
-        <h1 className="text-lg font-semibold text-center">{props.title}</h1>
-        <img src={props.imageUrl} alt="" className="w-full h-48 object-contain" />
-        
-        <p className="text-sm text-gray-500">Size:{props.size}</p>
-        <p className="text-sm text-gray-500">Quantity:{props.quantity}</p>
-       
-        <p className="text-sm text-gray-500">Price:{props.price}</p>
-        <p className="text-sm text-gray-500">Seller:{props.name}</p>
-        
-        
-        {/* <p className="text-sm text-gray-500">{props.size}</p>
-        <p className="text-sm text-gray-500">{props.quantity}</p>
-        <p className="text-sm text-gray-500">{props.rating}</p>
-        <p className="text-sm text-gray-500">{props.price}</p> */}
-        
-      </div>
+   <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+  <a className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+    <img className="object-cover" src={props.imageUrl} alt="product image" />
+    <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
+  </a>
+  <div className="mt-4 px-5 pb-5">
+    <a href="#">
+      <h5 className="text-xl tracking-tight text-slate-900">{props.title}</h5>
+    </a>
+    <div className="mt-2 mb-5 flex items-center justify-between">
+      <p>
+        <span className="text-3xl font-bold text-slate-900">₹{props.price}</span>
+        <span className="text-sm text-slate-900 line-through">{props.price}</span>
+      </p>
+     
     </div>
+    <a href="#" className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+      <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+      </svg>
+      Add to cart
+    </a>
+  </div>
+</div>
+
 
   );
 };
